5a3ac299debe6e7fad2fe6be82a566c462f8aada
addala harshad <64641316+addalaharshad@users.noreply.github.com>
2022-05-20 11:55:42 +0530

    Add files via upload

---
+++ BookMyShow-Application/BackEnd/Services/IMovies.cs
@@ -0,0 +1,13 @@
+﻿using BookMyShow_BE.Models;
+using Microsoft.AspNetCore.Mvc;
+using BookMyShow_BE.Data_Models;
+
+namespace BookMyShow_BE.Services
+{
+    public interface IMovies
+    {
+        public List<MovieDTO> GetMovies();
+        public List<String> GetMovie(int id);
+
+    }
+}
---
+++ BookMyShow-Application/BackEnd/Services/ITheaters.cs
@@ -0,0 +1,10 @@
+﻿using BookMyShow_BE.Models;
+using BookMyShow_BE.Data_Models;
+
+namespace BookMyShow_BE.Services
+{
+    public interface ITheaters
+    {
+        public List<TheaterDTO> GetTheaters(int id);
+    }
+}
