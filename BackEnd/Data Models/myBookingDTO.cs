0f32bfff14526d9068a01702768738e86f66c448
addala harshad <64641316+addalaharshad@users.noreply.github.com>
2022-06-07 14:18:13 +0530

    Add files via upload

---
+++ BackEnd/Data Models/myBookingDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BookMyShow_BE.Data_Models
+{
+    public class myBookingDTO
+    { 
+        public int MovieId { get; set; }
+        public int TheaterId { get; set; }
+        public int ShowId { get; set; }
+        public int SeatId { get; set; }
+    }
+}
---
+++ BackEnd/Models/Seat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookMyShow_BE.Models
+{
+    public partial class Seat
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int TheaterId { get; set; }
+        public int ShowId { get; set; }
+        public int SeatId { get; set; }
+    }
+}
